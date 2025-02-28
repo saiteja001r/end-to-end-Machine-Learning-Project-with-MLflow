5e5e10936361815e8ccbda49e3cd93278583794f
saiteja001r <saiteja001r@gmail.com>
2025-02-28 15:46:09 -0800

    updated files

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from mlProject import logger
 
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline 
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
+
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -18,6 +22,17 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
